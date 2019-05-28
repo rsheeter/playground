09e317cd82066bce6c7aefc5f2e762b3bf1f6326
Rod Sheeter <rsheeter@google.com>
2019-05-28 14:19:04 -0700

    Remove download, correct axis combination fn

--- axis_sizes/axis_sizes.py
+++ axis_sizes/axis_sizes.py
@@ -7,43 +7,21 @@ import io
 import itertools
 import logging
 import os
-import requests
+import re
 import traceback
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_boolean('registered_only', True, 'Whether to try leave non-registered tables ')
 flags.DEFINE_string('temp_dir', '/tmp/axis_sizes', 'Scratch directory')
-flags.DEFINE_boolean('download', True, 'Whether to try to download samples')
 flags.DEFINE_string('output_csv', '/tmp/axis_sizes.csv', 'Scratch directory')
+flags.DEFINE_string('filter', None, 'filename regex')
 
 def _font_dir():
   return os.path.join(FLAGS.temp_dir, 'fonts')
 
-def _vf_source_urls():
-  with open('vf_source_urls.txt', 'r') as f:
-    lines = [l.strip() for l in f.readlines() if not l.startswith('#')]
-  return [l for l in lines if l]
-
 def _local_file(font_url):
   return os.path.join(_font_dir(), font_url.rpartition('/')[2])
-
-def _download(url, local_file):
-  print('%s => %s' % (url, local_file))
-  response = requests.get(url)
-  if response.status_code != requests.codes.ok:
-    print('  DOWNLOAD FAILED with code %d' % response.status_code)
-    return False
-  os.makedirs(os.path.dirname(local_file), exist_ok=True)
-  with open(local_file, 'wb') as f:
-    f.write(response.content)
-  return True
-
-def _download_samples():
-  urls = _vf_source_urls()
-  local_files = [_local_file(u) for u in urls]
-  for url, local_file in zip(urls, local_files):
-    _download(url, local_file)
 
 def _ttFont(font_file):
   return ttLib.TTFont(font_file)
@@ -75,7 +53,7 @@ def _ignored_axes(font_file):
   return _all_axes(font_file) - _axes(font_file)
 
 def _axis_combinations(axes):
-  for i in range(len(axes)):
+  for i in range(len(axes) + 1):
     for axis_combination in itertools.combinations(axes, i):
       yield frozenset(axis_combination)
 
@@ -115,10 +93,9 @@ def _measure_sizes(font_file):
     yield (axes_retained, ttf_sz, woff2_sz)
 
 def _test_assets():
-  if FLAGS.download:
-    _download_samples()
-
   font_files = [os.path.join(_font_dir(), f) for f in os.listdir(_font_dir())]
+  if FLAGS.filter:
+    font_files = [f for f in font_files if re.search(FLAGS.filter, f)]
   return sorted([f for f in font_files if _isVF(f)])
 
 def _init_output():
@@ -140,13 +117,14 @@ def main(_):
 
   _init_output()
   for font_file in font_files:
-    size_by_axes = {axes:(ttf_sz, woff2_sz) for axes, ttf_sz, woff2_sz in _measure_sizes(font_file)}
     axes = _axes(font_file)
+    combinations = [c for c in _axis_combinations(axes)]
+    size_by_axes = {axes:(ttf_sz, woff2_sz) for axes, ttf_sz, woff2_sz in _measure_sizes(font_file)}
     for axis in sorted(axes):
       # print sizes between ever combination w/o axis and the same with it
       # purely for convenience in making spreadsheet downstream
-      for without_axis in _axis_combinations(axes - {axis}):
-        with_axis = without_axis | {axis}
+      for with_axis in [c for c in combinations if axis in c]:
+        without_axis = with_axis - {axis}
         size_before_ttf, size_before_woff2 = size_by_axes[with_axis]
         size_after_ttf, size_after_woff2 = size_by_axes[without_axis]
         _output_and_print('%s, "%s", %s, %d, %d, %d, %d' % (
