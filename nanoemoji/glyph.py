2bed5753f0c96e8ff700f98eb4880fcbfe0cad2b
rsheeter <rsheeter@google.com>
2020-04-04 12:46:41 -0700

    remove files that moved to standalone repos

--- nanoemoji/glyph.py
+++
@@ -1,6 +0,0 @@
-def glyph_name(codepoints):
-    try:
-        iter(codepoints)
-    except TypeError:
-        codepoints = [codepoints]
-    return "uni" + "_".join(("%04x" % c for c in codepoints))
